30aaf98350d38501bd5531c5f8ae24af4fd0b3bf
haronoid@gmail.com <haronoid@gmail.com>
2017-01-18 09:06:38 +0900

    add all file

---
+++ HelloWorld/HelloWorld/HelloWorldPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+using HelloWorld.Base;
+
+namespace HelloWorld
+{
+	public partial class HelloWorldPage : Base.PageBase
+	{
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			this.label1.Text = DateTime.Now.ToString();
+		}
+
+		public HelloWorldPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
